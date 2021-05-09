eea5d7f819471b72ca2047eeaf64cc9b485c4089
leithda <leithda@163.com>
2021-05-09 09:54:25 +0800

    注册中心与配置中心初始化

---
+++ study-mall/study-mall-service/order-service/src/main/java/cn/study/order/OrderServiceApplication.java
@@ -0,0 +1,16 @@
+package cn.study.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+}
---
+++ study-mall/study-mall-service/order-service/src/main/java/cn/study/order/controller/NacosController.java
@@ -0,0 +1,25 @@
+package cn.study.order.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试 Nacos 配置中心
+ */
+@RestController
+@RequestMapping("nacos")
+@RefreshScope
+public class NacosController {
+
+    @Value("${nacos.conf.version:0.0}")
+    private String version;
+
+
+    @GetMapping("version")
+    public String getVersion() {
+        return version;
+    }
+}
