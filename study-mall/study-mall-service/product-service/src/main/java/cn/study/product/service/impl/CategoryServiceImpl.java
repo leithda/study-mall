d1a6e71aff1a6765dcca232e46665230aac00d58
leithda <leithda@163.com>
2021-05-09 10:48:23 +0800

    业务代码编写

--- study-mall/study-mall-service/product-service/src/main/java/cn/study/product/service/impl/CategoryServiceImpl.java
+++ study-mall/study-mall-service/product-service/src/main/java/cn/study/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package cn.study.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +29,34 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    /**
+     * 获取分类树
+     * @param params 参数
+     */
+    @Override
+    public List<CategoryEntity> tree(Map<String, Object> params) {
+        List<CategoryEntity> categoryList = list();
+        List<CategoryEntity> rootCategoryList = categoryList.stream()
+                .filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                .sorted(Comparator.comparingInt(CategoryEntity::getSort))
+                .collect(Collectors.toList());
+
+        rootCategoryList.forEach(categoryEntity -> setCategoryChildren(categoryEntity, categoryList));
+        return rootCategoryList;
+    }
+
+    /**
+     * 设置分类的子节点
+     *
+     * @param parentCategory 父级分类
+     * @param categoryList   所有节点
+     */
+    private void setCategoryChildren(CategoryEntity parentCategory, List<CategoryEntity> categoryList) {
+        List<CategoryEntity> childrenCategoryList = categoryList.stream().filter(c -> c.getParentCid().equals(parentCategory.getCatId())).collect(Collectors.toList());
+        parentCategory.setChildren(childrenCategoryList);
+        childrenCategoryList.forEach(childCategory -> setCategoryChildren(childCategory, categoryList));
     }
 
 }
