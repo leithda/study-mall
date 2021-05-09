13155ce50c368c79e7e286dfbf13d065d4f8125b
leithda <leithda@163.com>
2021-05-09 09:54:25 +0800

    注册中心与配置中心初始化

---
+++ study-mall/study-mall-service/product-service/src/main/java/cn/study/product/ProductServiceApplication.java
@@ -0,0 +1,16 @@
+package cn.study.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProductServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+}
