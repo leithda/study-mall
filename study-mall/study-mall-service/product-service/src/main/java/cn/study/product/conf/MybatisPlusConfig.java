0c3606d9d1f274ee993b3fd9b6ea74a113784c84
leithda <leithda@163.com>
2021-05-12 23:18:18 +0800

    增加MybatisPlus设置

---
+++ study-mall/study-mall-service/product-service/src/main/java/cn/study/product/conf/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package cn.study.product.conf;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Mybatis Plus 配置
+ */
+@Configuration
+@EnableTransactionManagement // 开启事务
+@MapperScan("cn.study.product.dao")
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
