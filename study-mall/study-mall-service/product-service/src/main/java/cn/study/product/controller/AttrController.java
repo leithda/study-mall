249855d2c5d3ba9ec69c28e70c527a27d51585ba
leithda <leithda@163.com>
2021-05-15 00:28:07 +0800

    规格修改

--- study-mall/study-mall-service/product-service/src/main/java/cn/study/product/controller/AttrController.java
+++ study-mall/study-mall-service/product-service/src/main/java/cn/study/product/controller/AttrController.java
@@ -3,6 +3,7 @@ package cn.study.product.controller;
 import java.util.Arrays;
 import java.util.Map;
 
+import cn.study.product.entity.vo.AttrRespVo;
 import cn.study.product.entity.vo.AttrVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -54,8 +55,8 @@ public class AttrController {
     @RequestMapping("/info/{attrId}")
     public R info(@PathVariable("attrId") Long attrId){
 		AttrEntity attr = attrService.getById(attrId);
-
-        return R.ok().put("attr", attr);
+        AttrRespVo attrRespVo = attrService.getAttrInfo(attrId);
+        return R.ok().put("attr", attrRespVo);
     }
 
     /**
@@ -72,8 +73,8 @@ public class AttrController {
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody AttrEntity attr){
-		attrService.updateById(attr);
+    public R update(@RequestBody AttrVo attr){
+		attrService.updateAttr(attr);
 
         return R.ok();
     }
