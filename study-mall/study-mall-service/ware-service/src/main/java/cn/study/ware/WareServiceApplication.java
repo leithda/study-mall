1e585e57e13b2db1596c3b99b1703a6b3942cc9d
leithda <leithda@163.com>
2021-05-23 10:11:01 +0800

    整合仓储服务&获取仓库列表

--- study-mall/study-mall-service/ware-service/src/main/java/cn/study/ware/WareServiceApplication.java
+++ study-mall/study-mall-service/ware-service/src/main/java/cn/study/ware/WareServiceApplication.java
@@ -2,11 +2,13 @@ package cn.study.ware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 启动类
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WareServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(WareServiceApplication.class, args);
