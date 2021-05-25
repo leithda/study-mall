b122ccc2275d07f0c92985bcdf5dfe7a04eb7897
leithda <leithda@163.com>
2021-05-25 23:59:55 +0800

    分布式基础篇总结

--- study-mall/study-mall-service/product-service/src/main/java/cn/study/product/feign/CouponFeignService.java
+++ study-mall/study-mall-service/product-service/src/main/java/cn/study/product/feign/CouponFeignService.java
@@ -15,6 +15,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface CouponFeignService {
 
     /**
+     * 1、CouponFeignService.saveSpuBounds(spuBoundTo);
+     *      1）、@RequestBody将这个对象转为json。
+     *      2）、找到 coupon-service 服务，给/coupon/spubounds/save发送请求。
+     *          将上一步转的json放在请求体位置，发送请求；
+     *      3）、对方服务收到请求。请求体里有json数据。
+     *          (@RequestBody SpuBoundsEntity spuBounds)；将请求体的json转为SpuBoundsEntity；
+     * 只要json数据模型是兼容的。双方服务无需使用同一个to
      * 保存商品积分信息
      * @param spuBoundsTo 商品积分信息
      */
